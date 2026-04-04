5329db06e27dfe82d596b97e794bb3a00c01ce13
ManjunathNayak933 <112244799+ManjunathNayak933@users.noreply.github.com>
2026-04-05 01:25:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_TELEMETRY_DISABLED: '1',
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },

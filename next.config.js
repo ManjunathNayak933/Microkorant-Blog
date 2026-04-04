6c8811ede8ff50c03fea5ec20ec1c48e889174d8
ManjunathNayak933 <112244799+ManjunathNayak933@users.noreply.github.com>
2026-04-05 02:47:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: 'localhost' },
     ],
   },
 }
-
 module.exports = nextConfig
